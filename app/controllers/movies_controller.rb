461b2fa15408b2e508c3f6a48ab5dec3705fa188
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-08-20 22:21:25 -0600

    Add an index controller to get the top 20 rated movies.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,8 @@
 class MoviesController < ApplicationController
+  def index
+    return @movie_results = MovieData.top_20_rated_movies if params[:q]
+  end
+
   def show
     @movie_result = MovieResult.new(MovieData.all_movie_att(params[:id]))
   end
