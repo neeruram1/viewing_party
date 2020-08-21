579b51faa1c335a81b34c8bda5aa655897a68b81
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-08-20 22:16:05 -0600

    Test to get top 20 rated movies

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -6,10 +6,21 @@ RSpec.describe 'Discover Page' do
   end
 
   it "I see a button to discover top-rated movies" do
-    expect(page).to have_button('Find Top Rated Movies')
+    expect(page).to have_link('Find Top Rated Movies')
   end
 
   it "I see a text field to enter a keyword(s) to search by movie title" do
     find_button 'Find Movie'
+  end
+
+  it 'I see to 20 rated movies', :vcr do
+    click_on 'Find Top Rated Movies'
+
+    expect(current_path).to eq('/movies')
+    within '.top-rated' do
+      expect(page).to have_link('Gabriel\'s Inferno Part II')
+      expect(page).to have_link('Gabriel\'s Inferno')
+      expect(page).to have_link('Dilwale Dulhania Le Jayenge')
+    end
   end
 end
