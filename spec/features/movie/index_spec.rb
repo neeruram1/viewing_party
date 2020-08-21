5b011c3c138f876e708420add71d377d704bd124
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-08-21 16:31:54 -0600

    Add an edge case if the user entered a movie which doesn't exist.

--- spec/features/movie/index_spec.rb
+++ spec/features/movie/index_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Movies index page' do
   end
 
   it "I see the top rated movies button" do
-    expect(page).to have_button('Find Top Rated Movies')
+    expect(page).to have_link('Find Top Rated Movies')
   end
 
   it "I see the find movies form at the top of the page" do
@@ -23,5 +23,16 @@ RSpec.describe 'Movies index page' do
       expect(page).to have_content("Avengers: Infinity War")
       expect(page).to have_content("Vote Average: 8.3")
     end
+  end
+
+  it 'should show a message to the user', :vcr do
+    fill_in :search, with: 'qqwertyuiop[sdfghj'
+    click_on 'Find Movie'
+    expect(current_path).to eq('/movies')
+
+    within '.search-results' do
+      expect(page).to have_content("There was no match to the movie entered.")
+    end
+
   end
 end
