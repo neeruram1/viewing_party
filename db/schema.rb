22421a94f3745752150b000043eaa870c5ad787b
“Neeru <neeram85@gmail.com>
2020-08-24 18:33:47 -0600

    remove name column from user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_000159) do
+ActiveRecord::Schema.define(version: 2020_08_25_003211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 2020_08_25_000159) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.string "uid"
     t.datetime "created_at", null: false
