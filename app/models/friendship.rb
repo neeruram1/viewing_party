39bcaa0c8b9d94831d453480a091123b99e5be78
“Neeru <neeram85@gmail.com>
2020-08-26 12:10:19 -0600

    Added a validation to ensure you cannot add yourself as a friend

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,4 +2,11 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
   validates_uniqueness_of :friend, scope: :user, message: "This friend has already been added"
+  validate :disallow_self_referential_friendship
+
+  def disallow_self_referential_friendship
+    if friend_id == user_id
+      errors.add(:friend_id, 'Friend cannot refer back to yourself')
+    end
+  end
 end
